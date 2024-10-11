8f37fa1a3d71125298faa4111512e95f9c9e7974
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-10-11 23:42:28 +0500

    salom

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,8 @@
 const page = () => {
   return (
-    <div>page</div>
+    <div className="mt-11">About
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque culpa doloribus nesciunt molestias debitis, officiis provident omnis fugit quae consequatur aperiam non optio dignissimos nihil aut ullam nostrum sapiente alias. Deserunt nam eveniet non et quis autem quasi distinctio quam. Pariatur numquam minus ullam sequi enim! Soluta quam tenetur quibusdam.
+    </div>
   )
 }
 export default page
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,12 @@
-  "use client";
+"use client";
 import Link from "next/link";
-  const Navbar = () => {
+const Navbar = () => {
   return (
-    <div className="container gap[20px] mb-px[100] mb-20 shadow-lg mx-auto shadow-blue-900 grid gap-x-6 fixed gap-y-2 grid-cols-3 justify-between text-center px-4 py-8 bg-blue-200">
-        <Link href={'/hom'}>hom</Link>        
-        <Link href={'/contack'}>contack</Link>
-        <Link href={'/about'}>about</Link>
-
+    <div className="container gap[20px] mb-px[100] shadow-lg mx-auto shadow-blue-900 grid gap-x-6 fixed gap-y-2 grid-cols-3 justify-between text-center px-4 py-8 bg-blue-200">
+      <Link href={"/hom"}>Hom</Link>
+      <Link href={"/contack"}>Contack</Link>
+      <Link href={"/about"}>About</Link>
     </div>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;
