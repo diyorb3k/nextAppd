b13cfe30bda37bbff895832f3669aef298efc31d
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-10-11 14:39:07 +0500

    salom

--- src/app/hom/page.jsx
+++ src/app/hom/page.jsx
@@ -1,16 +1,34 @@
 'use client'
+
+import { useEffect, useState } from "react";
+
 const Hom = () => {
-  const[news, setNews]=usestate([])
+  const[news, setNews]=useState([])
+  console.log(news)
+  const apiKey = "22b3f60844aa4886b6e24407fa6213ec"; 
+  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`; 
   useEffect(()=>{
 const fetchNews = async()=>{
   try{
-    const response =await fetch()
+    const response =await fetch(url)
+    const data = await response.json()
+    setNews(data.articles)
+  }catch(error){
+    console.log('error')
   }
 }
-  })
+fetchNews()
+  },[])
   return (
     <div>
+{
 
+ news.map((articles)=>(
+  <div key={articles.id}>
+<p>{articles.description}</p>
+  </div>
+ )) 
+}
     </div>
   )
 }
