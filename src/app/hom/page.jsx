390ccb01ee279540eb485f5a6ce0a6edadbfcf2d
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-10-11 23:25:45 +0500

    salom

--- src/app/hom/page.jsx
+++ src/app/hom/page.jsx
@@ -21,7 +21,7 @@ const Hom = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 py-8 bg-blue-200">
+    <div className="container pt-20 mx-auto px-4 py-8 bg-blue-200">
       <h1 className="text-2xl font-bold mb-6 text-center">Latest News</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {news.map((article, index) => (
