850fe0fb35029969014c6d83a5e586a1b67f7f75
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-10-11 23:28:01 +0500

    salom

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
   const Navbar = () => {
   return (
-    <div className="container gap[20px] shadow-md mx-auto shadow-red-900 grid gap-x-6 fixed gap-y-2 grid-cols-3 justify-between text-center px-4 py-8 bg-blue-200">
+    <div className="container gap[20px] mb-px[100] shadow-lg mx-auto shadow-red-900 grid gap-x-6 fixed gap-y-2 grid-cols-3 justify-between text-center px-4 py-8 bg-blue-200">
         <Link href={'/hom'}>hom</Link>        
         <Link href={'/contack'}>contack</Link>
         <Link href={'/about'}>about</Link>
