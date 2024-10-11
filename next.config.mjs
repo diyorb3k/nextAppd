acef6fdf8ae16e089fc78151392da804893e1e8b
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-10-11 18:53:07 +0500

    dasd

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [
+        "image.cnbcfm.com",
+        "reuters.com",
+        "www.reuters.com",
+        "pagesix.com",
+        "ims.apnews.com",
+        "dims.apnews.com",
+        "deadline.com",
+        "hollywoodreporter.com",
+        "www.hollywoodreporter.com",
+        "mlbstatic.com",
+        "img.mlbstatic.com",  // Bu yangi domen
+        "scitechdaily.com",
+        "bbc.com",
+        "nintendolife.com",
+        "media.cnn.com",
+        "cloudinary.com",
+        "politico.com",
+        "washingtonpost.com",
+        "freep.com",
+        "s.w-x.co"
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/app/hom/page.jsx
+++ src/app/hom/page.jsx
@@ -1,35 +1,46 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 const Hom = () => {
-  const[news, setNews]=useState([])
-  console.log(news)
-  const apiKey = "22b3f60844aa4886b6e24407fa6213ec"; 
-  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`; 
-  useEffect(()=>{
-const fetchNews = async()=>{
-  try{
-    const response =await fetch(url)
-    const data = await response.json()
-    setNews(data.articles)
-  }catch(error){
-    console.log('error')
-  }
-}
-fetchNews()
-  },[])
+  const [movies, setMovies] = useState([]);
+  const query = "your search term"; // Define your search query here
+  const api_key = 'YOUR_API_KEY'; // Replace with your actual API key
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`;
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error("Error fetching movie data:", error);
+      }
+    };
+    fetchMovies();
+  }, [url]);
   return (
     <div>
-{
-
- news.map((articles)=>(
-  <div key={articles.id}>
-<p>{articles.description}</p>
-  </div>
- )) 
-}
+      {movies.map((movie) => (
+        <div key={movie.id}> 
+          <h2>{movie.title}</h2> {/* Display movie title */}
+          <p>{movie.overview}</p> {/* Display movie overview */}
+          {movie.poster_path && ( // Conditional rendering for the image
+            <Image 
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+              alt={movie.title} 
+              width={200} 
+              height={300} 
+            />
+          )}
+        </div>
+      ))}
     </div>
-  )
-}
-export default Hom
+  );
+};
+
+export default Hom;
