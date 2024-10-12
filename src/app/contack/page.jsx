797eab823cc304836d26b0bcea1d30bb462baed0
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-10-12 12:07:23 +0500

    salom

--- src/app/contack/page.jsx
+++ src/app/contack/page.jsx
@@ -1,6 +1,44 @@
-const contack = () => {
+"use client";
+import { useEffect, useState } from "react";
+
+const Contact = () => {
+  const [data, setData] = useState([]);
+  console.log(data);
+  const jobs = `https://www.arbeitnow.com/api/job-board-api`;
+
+  useEffect(() => {
+    const work = async () => {
+      try {
+        const response = await fetch(jobs);
+        const result = await response.json();
+        console.log(result); 
+        setData(result.data || []); 
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    work();
+  }, []);
+
   return (
-    <div className=" mx-auto px-5 pt-[100px]">contack</div>
-  )
-}
-export default contack
+    <div className="mx-auto px-5 pt-[100px]">
+      {data.length > 0 ? (
+        data.map((dassa, index) => (
+          <div key={index}>
+            <p>{dassa.company_name}</p>
+            <div
+              dangerouslySetInnerHTML={{ __html: dassa.description }}
+            ></div>
+            <p>{dassa.slug}</p>
+            <p>{dassa.location}</p>
+          </div>
+        ))
+      ) : (
+        <p>Ma'lumot topilmadi</p>
+      )}
+      <p>salom</p>
+    </div>
+  );
+};
+
+export default Contact;
